81e71c80c7cf8d585ebee373ea18facf303ce995
Gregory Brunner <gbrunner@esri.com>
2020-12-31 09:34:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,77 @@
-require([
-        "esri/Map",
-        "esri/views/MapView",
-        "esri/layers/Layer"
-      ], function (Map, MapView, Layer) {
-        var map = new Map({
-          basemap: "gray-vector"
-        });
+    require([
+      "esri/Map",
+      "esri/layers/FeatureLayer",
+      "esri/views/MapView",
+      "dojo/domReady!"
+    ], function(
+      Map,
+      FeatureLayer,
+      MapView
+    ) {
 
-        var view = new MapView({
-          map: map,
-          container: "viewDiv",
-          zoom: 7,
-          center: [-90, 40]
-        });
-
-        Layer.fromPortalItem({
-          portalItem: {
-            // autocasts as new PortalItem()
-            /*id: "c0f09a71805f4d45a6b6f3f1e0bdc7fc"*/
-            /*id: "af1ad38816814b7eba3fe74a3b84412d"*/
-            id: "c0238b8ae811443dbe52437496a1a514"
-          }
-        })
-          .then(addLayer)
-          .catch(rejection);
-
-        // Adds the layer to the map once it loads
-        function addLayer(layer) {
-          map.add(layer);
-        }
-
-        function rejection(error) {
-          console.log("Layer failed to load: ", error);
-        }
+      // Create the map
+      var map = new Map({
+        basemap: "gray"
       });
+
+      // Create the MapView
+      var view = new MapView({
+        container: "viewDiv",
+        map: map,
+        center:[-90, 38.5],
+        zoom: 9
+      });
+
+      /*************************************************************
+       * The PopupTemplate content is the text that appears inside the
+       * popup. {fieldName} can be used to reference the value of an
+       * attribute of the selected feature. HTML elements can be used
+       * to provide structure and styles within the content. The
+       * fieldInfos property is an array of objects (each object representing
+       * a field) that is use to format number fields and customize field
+       * aliases in the popup and legend.
+       **************************************************************/
+
+      var template = { // autocasts as new PopupTemplate()
+        title: "Neighborhood: {NHD_NAME}",
+        content: [{
+          // It is also possible to set the fieldInfos outside of the content
+          // directly in the popupTemplate. If no fieldInfos is specifically set
+          // in the content, it defaults to whatever may be set within the popupTemplate.
+          type: "fields",
+          fieldInfos: [{
+            fieldName: "NHD_NUMTXT",
+            label: "NHD Number and Text: ",
+            visible: true
+          }, {
+            fieldName: "NHD_NUM_ST",
+            label: "NHD_NUM_ST: ",
+            visible: true,
+            format: {
+              digitSeparator: true,
+              places: 0
+            }
+          }]
+        }]
+      };
+
+    /*var symbol = {
+      type: "picture-marker",  // autocasts as new PictureMarkerSymbol()
+      url: "https://graphicriver.img.customer.envatousercontent.com/files/224145068/preview.jpg?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=60d4fe57e0a4ead05318f4b9ba91192a",
+      width: "48px",
+      height: "48px"
+    };*/
+      /*var renderer = {
+      type: "simple",  // autocasts as new SimpleRenderer()
+      symbol: symbol
+    };*/
+      // Reference the popupTemplate instance in the
+      // popupTemplate property of FeatureLayer
+      var featureLayer = new FeatureLayer({
+        url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/STL_Neighborhood/FeatureServer",
+        outFields: ["*"],
+        popupTemplate: template
+      });
+      map.add(featureLayer);
+      
+    });
